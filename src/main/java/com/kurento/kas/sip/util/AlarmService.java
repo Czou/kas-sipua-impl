dfee3516aae4903bf383a5d2f1bf0b14bffdb5b8
Miguel París Díaz <mparisdiaz@gmail.com>
2013-05-29 18:02:53 +0200

    Check if there is a task for a uuid in AlarmService
    Change-Id: I9e7b0c642e53bf767def14da44c0818053c809d8

--- src/main/java/com/kurento/kas/sip/util/AlarmService.java
+++ src/main/java/com/kurento/kas/sip/util/AlarmService.java
@@ -1,11 +1,17 @@
 package com.kurento.kas.sip.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.IBinder;
 
 public class AlarmService extends Service {
+
+	private static final Logger log = LoggerFactory
+			.getLogger(AlarmService.class.getSimpleName());
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -19,11 +25,11 @@ public class AlarmService extends Service {
 
 			if (b != null) {
 				Integer uuid = b.getInt("uuid");
-				try {
-					AlarmUaTimer.getTaskTable().get(uuid).run();
-				} catch (Exception e) {
-					// Do nothing
-				}
+				KurentoUaTimerTask task = AlarmUaTimer.getTaskTable().get(uuid);
+				if (task != null)
+					task.run();
+				else
+					log.error("No task with uuid " + uuid);
 			}
 		}
 	}
